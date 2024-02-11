bba23f130a11155ba429cb9ff147a0ed00189451
TharunSai3579 <157801536+TharunSai3579@users.noreply.github.com>
2024-02-11 20:35:51 +0530

    Add files via upload

---
+++ passwordgenerator.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+length = int(input("Enter the length of the password you want : "))
+password = generate_password(length)
+print("password of your length:", password)
